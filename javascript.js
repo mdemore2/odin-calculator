1b044bf56720b50d44fad8972742ee4a46f9a44c
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-09-04 15:58:28 -0400

    Handling chained operators and negative numbers
    Added regex parsing and loops to handle multiple operators and negative results

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ function displayVal(e) {
 function calculate() {
     let display = document.querySelector('.display');
     let parsed = parseEquation(display.textContent);
-    display.textContent = Number(operate(parsed).toFixed(3));
+    display.textContent = Number(parsed.toFixed(3));
 
 }
 
@@ -40,12 +40,48 @@ function operate(expression){
 
 function parseEquation(equationString) {
     //let numbers = equationString.split('\+|\-|\/|\*');
-    //let operators = equationString.matchAll('\+|\-|\/|\*');
-    let expressions = equationString.matchAll(/([0-9]+)(\+|\-|\/|\*)([0-9]+)/g);
-    for (const match of expressions) {
-        console.log(match);
-        let expression = new Expression(match[2], Number(match[1]), Number(match[3]));
-        console.log(expression);
-        return expression;
+    let validExpressions = equationString.match(/\d+(?:\.?\d+)?(?:[-+*/]\d+(?:\.?\d+)?)+/g)[0];
+    console.log(validExpressions);
+    let operators = [...validExpressions.matchAll(/[-+*/]/g)];
+    console.log(operators[0]);
+    console.log(operators[0]['index']);
+    for(let i = 0; i < operators.length; i++){
+        let a = Number();
+        let b = Number();
+        if (i === 0){
+            a = validExpressions.slice(0, operators[i]['index']);
+            console.log(a);   
+        } else {
+            a = validExpressions.slice(operators[i-1]['index'], operators[i].index);
+            console.log(a);   
+        }
+        if (i+1 === operators.length) {
+            b = validExpressions.slice(operators[i]['index']+1, validExpressions.length);
+            console.log(b);
+        } else {
+            b = validExpressions.slice(operators[i]['index']+1, operators[i+1]['index'])
+            console.log(b);
+        }
+        let expression = new Expression(operators[i][0], Number(a), Number(b));
+        let result = operate(expression);
+        console.log(result);
+        validExpressions = validExpressions.replace(`${expression.a}${expression.operator}${expression.b}`, result);
+        console.log(validExpressions);
+        operators = [...validExpressions.matchAll(/[-+*/]/g)];
+        console.log(operators)
+        if (operators.length > 0) {
+            if (operators[0]['index'] != 0) {
+                i = -1;
+            }
+        }
     }
+
+    return Number(validExpressions);
+    //let expressions = equationString.matchAll(/([0-9]+)(\+|\-|\/|\*)([0-9]+)/g);
+    //for (const match of expressions) {
+    //    console.log(match);
+    //    let expression = new Expression(match[2], Number(match[1]), Number(match[3]));
+    //    console.log(expression);
+    //    return expression;
+    //}
 }
