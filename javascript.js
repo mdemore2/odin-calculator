e5d9ae448584892e9e64f4d1e23569192c91ddf4
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-09-04 14:56:53 -0400

    Implemented basic arithmetic functionality
    Added Expression class and basic string parsing and validation to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,30 @@
 function displayVal(e) {
-    display = document.querySelector('.display');
-    display.textContent = display.textContent + e.currentTarget.textContent;
+    let display = document.querySelector('.display');
+    if (e.currentTarget.textContent === 'C') {
+        display.textContent = '';
+    } else {
+        display.textContent = display.textContent + e.currentTarget.textContent;
+    }
 }
 
 function calculate() {
+    let display = document.querySelector('.display');
+    let parsed = parseEquation(display.textContent);
+    display.textContent = Number(operate(parsed).toFixed(3));
 
 }
 
-function operate(operator, a, b){
-    switch(operator){
+function Expression(operator, a, b){
+    this.operator = operator;
+    this.a = a;
+    this.b = b;
+}
+
+function operate(expression){
+    let a = expression.a;
+    let b = expression.b;
+
+    switch(expression.operator){
         case '+':
             return a + b;
         case '-':
@@ -19,5 +35,17 @@ function operate(operator, a, b){
             return a * b;
         default:
             return 'Invalid Operation';
+    }
+}
+
+function parseEquation(equationString) {
+    //let numbers = equationString.split('\+|\-|\/|\*');
+    //let operators = equationString.matchAll('\+|\-|\/|\*');
+    let expressions = equationString.matchAll(/([0-9]+)(\+|\-|\/|\*)([0-9]+)/g);
+    for (const match of expressions) {
+        console.log(match);
+        let expression = new Expression(match[2], Number(match[1]), Number(match[3]));
+        console.log(expression);
+        return expression;
     }
 }
